32ac433458c58a342a72742ce691e2c345c860c6
ericnorris <erictnorris@gmail.com>
2014-12-01 21:47:54 -0500

    Implemented open().

--- src/readable-cdb.js
+++ src/readable-cdb.js
@@ -2,16 +2,16 @@
 
 var readable = function(file) {
     // constructor
-}
+};
 
 readable.prototype.open = function(callback) {
     // TODO
 };
 
-readable.prototype.get = function(key, offset, callback) {
+readable.prototype.get = function(key, callback) {
     // TODO
-}
+};
 
 readable.prototype.close = function(callback) {
     // TODO
-}
+};
--- src/writable-cdb.js
+++ src/writable-cdb.js
@@ -1,20 +1,44 @@
-var EventEmitter = require('events').EventEmitter;
+var events = require('events');
+var fs = require('fs');
 
-var writable = function(file) {
-    // constructor
-}
+// Constants
+var HEADER_SIZE = 2048;
+
+// Writable CDB definition
+var writable = module.exports = function(file) {
+    this._file = file;
+};
+
+// extend EventEmitter for emit()
+util.inherits(writable, events.EventEmitter);
 
 writable.prototype.open = function(callback) {
-    // TODO
+    var recordStream = fs.createWriteStream(this._file, {start: HEADER_SIZE}),
+        callback = callback || function() {},
+        self = this;
+
+    // Set up event handlers
+    function fileOpened(fd) {
+        self._recordStream = recordStream;
+        self._filePosition = HEADER_SIZE;
+        self.emit('open');
+        callback();
+    }
+
+    function error(err) {
+        self.emit('error', err);
+        callback(err);
+    }
+
+    // Listen for events for record stream
+    recordStream.once('open', fileOpened);
+    recordStream.once('error', error);
 };
 
 writable.prototype.put = function(key, data, callback) {
     // TODO
-}
+};
 
 writable.prototype.close = function(callback) {
     // TODO
-}
-
-// extend EventEmitter for emit()
-util.inherits(writable, EventEmitter);
+};
